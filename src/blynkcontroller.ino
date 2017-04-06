3d90825136a1c91432ca30c39e604549d020dbaf
Ben Thompson <thomp211@wwu.edu>
2017-04-05 23:28:24 -0700

    interrupt not working. using pullup input

--- src/blynkcontroller.ino
+++ src/blynkcontroller.ino
@@ -1,49 +1,57 @@
 //Tilt sensor activates state change and powers on
 //D1 Mini
 
-//Settings   
-#define tilt_wait   500
+//Settings
+#define tilt_wait   1000
 #define tiltPin 2
-#define powerPin 12
+#define powerPin 4
 
-//Status Variables   
-uint8_t prev_state;
-long last_msg = 0;
-int tilt_state;
+//Status Variables
+volatile long last_msg = 0;
+volatile int tilt_state;
 bool msg_sent = false;
 bool send_shutdown = false;
 
+void door_close_isr(){
+  //Wait for tilt status check
+  long now = millis();
+  if((now - last_msg) > tilt_wait){
+    
+    last_msg = millis();
+    Serial.println(digitalRead(tiltPin));
+    
+    Serial.println("Door Closed.");
+    digitalWrite(powerPin, LOW);
+  }
+}
+
+void door_open_isr(){
+  //Wait for tilt status check
+  long now = millis();
+  if((now - last_msg) > tilt_wait){
+    
+    last_msg = millis();
+    Serial.println(digitalRead(tiltPin));
+    Serial.println("Door Opened.");
+    digitalWrite(powerPin, LOW);
+  }
+}
+
+
 void setup() {
-  
+
   Serial.begin(9600);
 
   //Tilt
-  pinMode(tiltPin, INPUT);
+  pinMode(tiltPin, INPUT_PULLUP);
   pinMode(powerPin, OUTPUT);
+
+  //Interupt
+  attachInterrupt(digitalPinToInterrupt(tiltPin), door_open_isr, FALLING);
+  attachInterrupt(digitalPinToInterrupt(tiltPin), door_close_isr, RISING);
+
 }
 
 void loop() {
 
-  //Wait for tilt status check
-  long now = millis();
-  if((now - last_msg) > tilt_wait){
-    tilt_state = digitalRead(tiltPin);
-    last_msg = millis();
-
-    //check for new tilt status
-    if (tilt_state != prev_state) {
-      
-      //Serial
-      if(tilt_state){
-        Serial.print("Door Open.");
-        digitalWrite(powerPin, HIGH);
-      }else{
-
-        Serial.println("Door Closed. Shutting down circuit...");
-        delay(2000);
-        digitalWrite(powerPin, LOW);
-      }
-      prev_state = tilt_state;
-    }
-  }
 }
