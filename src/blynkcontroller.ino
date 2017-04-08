b8ac0a28b0282cf425ce01b79efd9b266c1b6771
Ben Thompson <thomp211@wwu.edu>
2017-04-07 23:59:57 -0700

    deep sleep

--- src/blynkcontroller.ino
+++ src/blynkcontroller.ino
@@ -1,17 +1,41 @@
 //Tilt sensor activates state change and powers on
 //D1 Mini
 
+//TODO: enable wifi for a given amount of time (turn on D4) to allow
+//      wifi to push change "New Mail", or "Mailbox Empty"
+
+#include <SimpleTimer.h>
+
 //Settings
-#define tilt_wait   1000
-#define in_pin 2
-#define out_pin 3
-#define powerPin 4
+#define tilt_wait   1000 //1s
+#define wifi_wait  10000 //10s
+#define sleep_wait  1000 //1s
+#define in_pin         2
+#define out_pin        3
+#define esp_on_pin    12
 
 //Status Variables
-volatile long last_msg = 0;
+volatile long last_msg =      0;
 volatile bool mail_empty = true;
-// bool msg_sent = false;
-// bool send_shutdown = false;
+
+SimpleTimer timer;
+
+
+void wifiOnInterval(){
+  Serial.println("Turning on esp wifi pin for 10 secs");
+  digitalWrite(esp_on_pin, HIGH);
+  timer.setTimeout(wifi_wait, wifiShutoff);
+  
+}
+
+void wifiShutoff(){
+  Serial.println("esp shutoff.");
+  digitalWrite(esp_on_pin, LOW);
+}
+
+void deepSleep(){
+  
+}
 
 void in_isr(){
   //Wait for tilt status check
@@ -21,8 +45,9 @@ void in_isr(){
   if((now - last_msg) > tilt_wait && mail_empty == true){
     last_msg = millis();
     Serial.println("New Mail!");
-    digitalWrite(powerPin, LOW);
     mail_empty = false;
+    wifiOnInterval();
+    
   }
 
 }
@@ -34,30 +59,31 @@ void out_isr(){
   //waits for tiltwait
   if((now - last_msg) > tilt_wait && mail_empty == false){
     last_msg = millis();
-
     Serial.println("Obtained.");
-    digitalWrite(powerPin, LOW);
-
     mail_empty = true;
+    wifiOnInterval();
   }
 }
 
 
 void setup() {
-
+  
   Serial.begin(9600);
-
+  
   //Tilt
   pinMode(in_pin, INPUT_PULLUP);
   pinMode(out_pin, INPUT_PULLUP);
-  pinMode(powerPin, OUTPUT);
+  pinMode(esp_on_pin, OUTPUT);
 
   //Interupt
   attachInterrupt(digitalPinToInterrupt(in_pin), in_isr, RISING);
   attachInterrupt(digitalPinToInterrupt(out_pin), out_isr, RISING);
+
+  //timer
+  timer.setInterval(sleep_wait, deepSleep());
   
 }
 
 void loop() {
-
+  timer.run();
 }
